f06b66b994763cd665ee200a61c23e75bf71b4d6
Rithvik Nishad <rithvikn2001@gmail.com>
2022-01-12 23:50:14 +0530

    updates firmware TODOs

--- Smart Meter ESP32 Firmware/Smart Meter ESP32 Firmware.ino
+++ Smart Meter ESP32 Firmware/Smart Meter ESP32 Firmware.ino
@@ -5,11 +5,25 @@
 */
 
 // the setup function runs once when you press reset or power the board
-void setup() {
-    
+void setup()
+{
+    // TODO: Intiialize GPIOs and IIC bus devices
+
+    // TODO: load configuration files
+
+    // TODO: verify configuration
+
+    // TODO: initialize network - WiFi AP
+
+    // TODO: initialize MQTT PubSub Client
+
+    // TODO: mqtt.setWillMessage(<associated_service_topic>/online, false)
+
+    // TODO: Inform GridServer about meter-online
 }
 
 // the loop function runs over and over again until power down or reset
-void loop() {
-    
+void loop()
+{
+    // TODO: create application specific event loop using millis() mod interval method.
 }
