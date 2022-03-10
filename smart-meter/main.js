4ad56943ce10f99f4da91da939f054c5a6222518
krithikabs <krithika27122001@gmail.com>
2022-03-10 17:26:42 +0530

    Logs PV_Estimate

--- smart-meter/main.js
+++ smart-meter/main.js
@@ -1,35 +1,32 @@
-let print = console.log
+let print = console.log;
 
-const https = require('https')
+const https = require("https");
 const options = {
-  hostname: 'api.solcast.com.au',
+  hostname: "api.solcast.com.au",
   port: 443,
-  path: '/world_pv_power/estimated_actuals?latitude=11.749142&longitude=75.489035&capacity=5&tilt=12&azimuth=180&hours=168&format=json&api_key=LscmE7tIq2Jh44XyAgLCuTmgXP-Ce8eG',
-  method: 'GET'
-}
+  path: "/world_pv_power/estimated_actuals?latitude=11.749142&longitude=75.489035&capacity=5&tilt=12&azimuth=180&hours=168&format=json&api_key=LscmE7tIq2Jh44XyAgLCuTmgXP-Ce8eG",
+  method: "GET",
+};
 
-const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
-  
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
   var bodyChunks = [];
-  res.on('data', function(chunk) {
-    // You can process streamed parts here...
-    bodyChunks.push(chunk);
-  }).on('end', function() {
-    var body = Buffer.concat(bodyChunks);
-   
-    let data = JSON.parse(body)
-    console.log(data)
-    // ...and/or process the entire body here.
-  })
+  res
+    .on("data", function (chunk) {
+      // You can process streamed parts here...
+      bodyChunks.push(chunk);
+    })
+    .on("end", function () {
+      var body = Buffer.concat(bodyChunks);
+
+      let data = JSON.parse(body);
+      data.estimated_actuals.forEach((row) => {
+        console.log(row.period_end + "-" + row.pv_estimate);
+      });
+    });
 });
-  
-      // convert json string to js object
-      // iterate through each data of estimated_actuals and print it
-      
 
- 
+req.on("error", console.error);
 
-req.on('error', console.error)
-
-req.end()
+req.end();
