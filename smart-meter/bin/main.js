e8071187327cc80841191456214961710d26e9e4
rithviknishad <rithvikn2001@gmail.com>
2022-03-12 16:49:13 +0530

    fix args

--- smart-meter/bin/main.js
+++ smart-meter/bin/main.js
@@ -1,10 +1,11 @@
 const yargs = require("yargs");
 
-const options = yargs.usage("Usage: -n <name>").option("n", {
-    alias: "name",
-    describe: "Your name",
+const options = yargs.usage("Usage: prosumer").option("a", {
+    alias: "prosumer-id",
+    default: "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+    describe: "Prosumer EP Address",
     type: "string",
-    demandOption: true,
+    demandOption: false,
 }).argv;
 
 const greeting = `Hello, ${options.name}!`;
