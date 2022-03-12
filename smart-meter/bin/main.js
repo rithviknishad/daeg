28325d59aa0c3cb0228eb1a87070cad46122d4dd
rithviknishad <rithvikn2001@gmail.com>
2022-03-12 16:11:52 +0530

    npm init

---
+++ smart-meter/bin/main.js
@@ -0,0 +1,12 @@
+const yargs = require("yargs");
+
+const options = yargs.usage("Usage: -n <name>").option("n", {
+    alias: "name",
+    describe: "Your name",
+    type: "string",
+    demandOption: true,
+}).argv;
+
+const greeting = `Hello, ${options.name}!`;
+
+console.log(greeting);
