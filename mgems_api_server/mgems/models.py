b2e4fb1a94911ef2c1b02d282404e0339852c943
rithviknishad <rithvikn2001@gmail.com>
2022-03-13 17:09:08 +0530

    [MGEMS-SERVER]: adds prosumer and grid models

--- mgems_api_server/mgems/models.py
+++ mgems_api_server/mgems/models.py
@@ -2,27 +2,76 @@ from django.db import models
 from math import radians, cos, sin, asin, sqrt
 
 
-class GeoLocation(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+class _ModelMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
 
     class Meta:
-        unique_together = "latitude", "longitude"
+        abstract = True
 
-    @staticmethod
-    def distance_between(a: "GeoLocation", b: "GeoLocation"):
-        """Finds the ground distance between two geographic location."""
-        lat1, lon1, lat2, lon2 = radians(a.latitude), radians(a.longitude), radians(b.latitude), radians(b.longitude)
+    def get_fields(self):
+        return [(field.verbose_name, field.value_from_object(self)) for field in self.__class__._meta.fields]
 
-        # Haversine formula
-        dlon = lon2 - lon1
-        dlat = lat2 - lat1
-        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
 
-        c = 2 * asin(sqrt(a))
 
-        # Radius of earth in kilometers. Use 3956 for miles
-        r = 6371
+class Microgrid(_ModelMixin, models.Model):
+    prosumer = models.ForeignKey("Prosumer", on_delete=models.PROTECT)
+    parent_grid = models.ForeignKey("Grid", on_delete=models.PROTECT)
 
-        # calculate the result
-        return c * r
+
+class Prosumer(_ModelMixin, models.Model):
+    id = models.TextField(max_length=39, unique=True, primary_key=True)
+    grid = models.ForeignKey(Microgrid, on_delete=models.PROTECT)
+    max_import_power = models.FloatField()
+    max_export_power = models.FloatField()
+    is_online = models.BooleanField()
+    is_trader = models.BooleanField()
+    is_dr_adaptive = models.BooleanField()
+    # TODO: add location field
+    # TODO: add nearest node field
+
+    @property
+    def vp_address(self):
+        return self.id
+
+    @property
+    def registered_on(self):
+        return self.created_at
+
+
+class __ProsumerSubComponentMixin(_ModelMixin, models.Model):
+    prosumer = models.ForeignKey(Prosumer, on_delete=models.PROTECT)
+    is_online = models.BooleanField()
+
+    class Meta:
+        abstract = True
+
+
+class DemandResponseAdaptiveEntityMixin(models.Model):
+    dr_adaptive = models.BooleanField()
+
+    class Meta:
+        abstract = True
+
+
+class ProsumerEnergyGenerationSystem(__ProsumerSubComponentMixin, models.Model):
+    max_power = models.FloatField()
+    is_renewable = models.BooleanField()
+    type = models.TextField()
+    efficiency = models.FloatField()
+
+
+class ProsumerEnergyConsumptionSystem(__ProsumerSubComponentMixin, DemandResponseAdaptiveEntityMixin, models.Model):
+    max_power = models.FloatField()
+
+
+class ProsumerEnergyStorageSystem(__ProsumerSubComponentMixin, DemandResponseAdaptiveEntityMixin, models.Model):
+    max_capacity = models.FloatField()
+    usable_capacity = models.FloatField()
+    max_charge_rate = models.FloatField()
+    max_discharge_rate = models.FloatField()
+    type = models.TextField()
