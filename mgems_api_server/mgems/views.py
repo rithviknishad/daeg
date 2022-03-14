ee4804345c414c0998592dba8e5188aeba136c52
rithviknishad <rithvikn2001@gmail.com>
2022-03-14 12:58:13 +0530

    updates views

--- mgems_api_server/mgems/views.py
+++ mgems_api_server/mgems/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django_filters.rest_framework import (
+    DjangoFilterBackend,
+    FilterSet,
+    CharFilter,
+    BooleanFilter,
+    DateTimeFromToRangeFilter,
+    ChoiceFilter,
+    NumberFilter,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.serializers import ModelSerializer
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-# Create your views here.
+from mgems.models import Prosumer
+
+
+class ProsumerSerialzier(ModelSerializer):
+    """Serializer for Prosumer"""
+
+    class Meta:
+        model = Prosumer
+        fields = ("id", "max_import_power", "max_export_power", "is_online", "is_trader", "is_dr_adaptive")
+
+
+class ProsumerFilter(FilterSet):
+    """Filter sets for Prosumers"""
+
+    id = NumberFilter()
+
+
+class ProsumerViewSet(ModelViewSet):
+    """Model View Set for Prosumer"""
+
+    queryset = Prosumer.objects.all()
+    serializer_class = ProsumerSerialzier
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = ProsumerFilter
+
+    def get_queryset(self):
+        return super().get_queryset()
