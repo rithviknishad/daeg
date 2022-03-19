0d9b5eadde79abc68a9cf5ba1a1ca6cf1f4f15f8
rithviknishad <rithvikn2001@gmail.com>
2022-03-19 21:48:42 +0530

    [smart-meter] refactors

--- smart-meter/bin/main.js
+++ smart-meter/bin/main.js
@@ -1,24 +1,15 @@
 require("dotenv").config();
 
-const mqtt = require("mqtt");
 const chalk = require("chalk");
-
-/**
- * The logger to be used.
- */
-const logger = eval(process.env.LOGGER || "console.log");
-const fmtMsg = (msg) => `[${new Date().toISOString()}] ${msg}`;
-const log = (msg) => logger(fmtMsg(msg));
-const trace = (msg) => logger(chalk.gray(fmtMsg(msg)));
-const warn = (msg) => logger(chalk.yellowBright(fmtMsg(msg)));
-const error = (msg) => logger(chalk.redBright(fmtMsg(msg)));
-const success = (msg) => logger(chalk.greenBright(fmtMsg(msg)));
+const mqtt = require("mqtt");
+const log = require("./logger");
+const solar_forecast = require("./solar_forecast");
 
 /**
  * Vaidyuti Protocol Address assigned to the prosumer.
  */
 const VP_ADDRESS = process.env.VP_ADDRESS || "no-vp-addr";
-trace(`Starting prosumer with VP_ADDRESS=${VP_ADDRESS}`);
+log.trace(`Starting prosumer with VP_ADDRESS=${VP_ADDRESS}`);
 
 /**
  * The speed at which the prosumer meters clock should run.
@@ -28,7 +19,7 @@ trace(`Starting prosumer with VP_ADDRESS=${VP_ADDRESS}`);
  * Note: this will affect the `UPDATE_INTERVAL`s effective value.
  */
 const SIMULATION_SPEED = process.env.SIMULATION_SPEED || 1;
-trace(`Simulation speed set to: ${SIMULATION_SPEED}x`);
+log.trace(`Simulation speed set to: ${SIMULATION_SPEED}x`);
 
 /**
  * The interval at which contracts should be exchanged and communicated with
@@ -38,7 +29,7 @@ trace(`Simulation speed set to: ${SIMULATION_SPEED}x`);
  */
 const UPDATE_INTERVAL =
   (process.env.UPDATE_INTERVAL || 900000) / SIMULATION_SPEED;
-trace(
+log.trace(
   `Contracts will be exchanged every ${
     UPDATE_INTERVAL / 1000
   } seconds (real world clock).`
@@ -69,7 +60,7 @@ const MQTT_PASSWORD = `${VP_ADDRESS}`;
 // TODO: register prosumer to MGEMS server using HTTP POST
 // TODO: gracefully terminate with exit code 1, if response != OK
 
-log(`Connecting to ${MGEMS_MQTT_URL}`);
+log.log(`Connecting to ${MGEMS_MQTT_URL}`);
 const client = mqtt.connect(MGEMS_MQTT_URL, {
   clientId: MQTT_CLIENT_ID,
   username: MQTT_USERNAME,
@@ -82,21 +73,21 @@ const client = mqtt.connect(MGEMS_MQTT_URL, {
 
 client
   .on("connect", () => {
-    success(`Connected to ${MGEMS_MQTT_URL}`);
+    log.success(`${chalk.yellowBright("⚡")} Connected to ${MGEMS_MQTT_URL}`);
   })
   .on("disconnect", () => {
-    error("MQTT client disconnected.");
+    log.error("MQTT client disconnected.");
   })
   .on("close", () => {
-    error("MQTT connection closed.");
+    log.error("MQTT connection closed.");
   })
   .on("end", () => {
-    error("MQTT connection ended.");
+    log.error("MQTT connection ended.");
   });
 
 function gracefullyExit() {
-  logger("");
-  warn("Prosumer Meter interrupted. Gracefully exiting...");
+  log.logger("");
+  log.warn("Prosumer Meter interrupted. Gracefully exiting...");
   client.end(false, {}, () => {
     process.exit();
   });
