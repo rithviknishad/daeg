bba345576716bd7e3b3894ec9c8f2d62d735e2fb
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-06 09:11:35 +0530

    entry point

--- prosumer_meter_profile_generator/bin/prosumer_meter_profile_generator.dart
+++ prosumer_meter_profile_generator/bin/prosumer_meter_profile_generator.dart
@@ -1,3 +1,101 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
+import 'package:http/http.dart' as http;
+
+import 'solar_estimate.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  final runner = CommandRunner(
+    'sm-primer',
+    "Batch energy consumption and generation profile management tool for Smart Meter.",
+  );
+
+  final parser = ArgParser()
+    ..addOption(
+      'output',
+      help: "The path of the output CSV file",
+      defaultsTo: "output.csv",
+      valueHelp: "<prosumer_id>.csv",
+    )
+    ..addOption(
+      'interval',
+      help: "Time delta of data points in seconds",
+      defaultsTo: "900",
+      valueHelp: "900",
+    )
+    ..addOption(
+      'source-load-profile',
+      help: "The path to load profile",
+      defaultsTo: "load_profile.csv",
+      valueHelp: "path/to/load_profile.csv",
+    )
+    ..addOption('latitude', defaultsTo: "11.3")
+    ..addOption('longitude', defaultsTo: "74.6")
+    ..addOption('solar-capacity', defaultsTo: "5")
+    ..addCommand('cache', cacheCommand);
+
+  final solcastApiKey = Platform.environment["SOLCAST_API_KEY"];
+  if (solcastApiKey == null) {
+    stderr.writeln("Missing SOLCAST_API_KEY in environment. Shall terminate.");
+    exit(1);
+  }
+
+  runner.run(arguments).catchError((error) {
+    if (error is! UsageException) throw error;
+    print(error);
+    exit(64); // Exit code 64 indicates a usage error.
+  });
+
+  // fetchSolarEstimates(11.3, 74.2, 5, solcastApiKey);
+}
+
+class CacheCommand extends Command {
+  @override
+  final name = "cache";
+
+  @override
+  final description = "Cache solar profiles using Solcast API and save as CSV.";
+
+  CacheCommand() {
+    argParser
+      ..addOption(
+        "api-key",
+        help: "Solcast API Key",
+        valueHelp: Platform.environment["SOLCAST_API_KEY"],
+        defaultsTo: Platform.environment["SOLCAST_API_KEY"],
+      )
+      ..addOption("latitude", mandatory: true)
+      ..addOption("longitude", mandatory: true)
+      ..addOption(
+        "capacity",
+        help: "Installed capacity of PV system in KW",
+        valueHelp: "5  # signifies 5 KW PV System",
+        defaultsTo: "1",
+      );
+  }
+}
+
+Future<void> fetchSolarEstimates(
+  double latitude,
+  double longitude,
+  double capacity,
+  String solcastApiKey,
+) async {
+  final res = await http.get(Uri.parse(
+      'https://api.solcast.com.au/world_pv_power/estimated_actuals?latitude=$latitude&longitude=$longitude&capacity=$capacity&tilt=12&azimuth=180&hours=168&format=json&api_key=$solcastApiKey'));
+
+  final data = jsonDecode(res.body);
+
+  final records = data['estimated_actuals'];
+
+  for (final record in records) {
+    print(SolarEstimate.fromJson(record));
+  }
+}
+
+double lerpDouble(num a, num b, double t) {
+  return a + (b - a) * t;
 }
