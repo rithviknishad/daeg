d7c37924346e36a4088c98d0d865c9d188cd21a1
Rithvik Nishad <rithvikn2001@gmail.com>
2022-04-09 18:13:52 +0530

    improve code reusability

--- contract-exporter/main.py
+++ contract-exporter/main.py
@@ -10,37 +10,71 @@ from typing import Final
 
 @dataclass
 class Transaction(ABC):
-    """Abstract class Transaction provides a common base for Atomic and Aggregated Transaction."""
+    """
+    Abstract class Transaction provides a common base for Atomic and Aggregated
+    Transaction.
+    """
 
     id: str
-    """The unique key used to identify an atomic transaction in MGEMS database."""
+    """
+    The unique key used to identify an atomic transaction in MGEMS database.
+    """
 
     opened_at: datetime
-    """The date and time at which energy exchange has started."""
+    """
+    The date and time at which energy exchange has started.
+    """
 
     closed_at: datetime
-    """The date and time at which energy exchange has stopped."""
+    """
+    The date and time at which energy exchange has stopped.
+    """
+
+    def parse(json_data: dict) -> "Transaction":
+        # TODO: @rajasri parse the transaction and return an instance of `AtomicTransaction` or `AggregateTransaction` appropriatley.
+        raise NotImplementedError()
+
+    def export(self, filename: str = "transaction.pdf") -> None:
+        # TODO: @rajasri export this transaction (`self`) to a pdf file whose file name is specified by `filename`.
+        raise NotImplementedError()
 
 
 @dataclass
 class AtomicTransaction(Transaction):
     producer_address: str
-    """The Vaidyuti Protocol Address of the producer who has attempted to provide energy to a consumer."""
+    """
+    The Vaidyuti Protocol Address of the producer who has attempted to provide
+    energy to a consumer.
+    """
 
     consumer_address: str
-    """The Vaidyuti Protocol Address of the consumer who has received energy from a producer."""
+    """
+    The Vaidyuti Protocol Address of the consumer who has received energy from a
+    producer.
+    """
 
     energy_consumed: float
-    """The amount of energy consumed by the consumer in kWh. Transmission and distribution lossess are not accounted in this attribute."""
+    """
+    The amount of energy consumed by the consumer in kWh. Transmission and
+    distribution lossess are not accounted in this attribute.
+    """
 
     transmission_lossess: float
-    """The estimated amount of energy lost in transmission and distribution in kWh."""
+    """
+    The estimated amount of energy lost in transmission and distribution in kWh.
+    """
 
     consumption_unit_price: float
-    """The per unit price thats levied on the amount of energy directly consumed by the consumer."""
+    """
+    The per unit price thats levied on the amount of energy directly consumed by
+    the consumer.
+    """
 
     lossess_unit_price: float
-    """The per unit price thats levied on the estimated amount of energy lost in transmission and distribution layers."""
+    """
+    The per unit price thats levied on the estimated amount of energy lost in 
+    transmission and distribution layers.
+    """
 
 
 class AggregateTransaction(Transaction):
@@ -48,35 +82,14 @@ class AggregateTransaction(Transaction):
     pass
 
 
-def export_transaction_to_pdf(transaction: Transaction, output_file_name: str = "transaction.pdf"):
-    # TODO: @rajasri export the `transaction` to a pdf file whose file name is specified by `output_file_name`.
-    pass
+def handle_no_transaction_json_file_specified() -> None:
+    print("Specify transaction to be exported (eg: ./main.py transaction.json)")
+    exit(1)
 
 
 def main():
-    transaction_json_file: str = " ".join(sys.argv[1:0])
-    if not transaction_json_file:
-        print("Specify a transaction file to be exported to PDF. Eg: ./main.py transaction.json")
-        exit(1)
-    output_file_name: Final = "transaction.pdf"
-    transaction: Transaction
-
-    with open(transaction_json_file) as json_file:
-        json_data: dict = json.load(json_file)
-
-        print("read >> ", json_data)  # TODO: @rajasri remove this line once ready for production.
-
-        # TODO: @rajasri parse `Transaction` from `transaction_json` and assign to `transaction` variable
-        # transaction = ...
-
-    # TODO: @rajasri remove the following line and wrap it within a try except block once ready for production.
-    export_transaction_to_pdf(transaction, output_file_name)
-
-    # TODO: @rajasri uncomment the following lines once ready for production.
-    # try:
-    #     export_transaction_to_pdf(transaction, output_file_name)
-    # except:
-    #     print("Something went wrong while exporting to pdf")
+    with open(" ".join(sys.argv[1:]) or handle_no_transaction_json_file_specified()) as json_file:
+        Transaction.parse(json.load(json_file)).export()
 
 
 if __name__ == "__main__":
